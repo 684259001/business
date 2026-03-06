3d9756d17cf4fe37e01f11a54ab22c121133293b
Kama <684259001@webmail.npru.ac.th>
2026-03-06 15:57:35 +0700

    Add files via upload

---
+++ select0.php
@@ -0,0 +1,11 @@
+select0.php
+
+
+<?php
+require "connect.php";
+$sql = "SELECT * FROM customer";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($result);
+?>
